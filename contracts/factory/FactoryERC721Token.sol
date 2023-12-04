32ee1770ff4a1b1682b920806caf6782d17bc780
Jonathan Nolasco <jnolascob@gmail.com>
2023-12-04 07:48:32 -0500

    Format code

--- contracts/factory/FactoryERC721Token.sol
+++ contracts/factory/FactoryERC721Token.sol
@@ -4,23 +4,36 @@ pragma solidity ^0.8.20;
 import "../base/ERC721Token.sol";
 
 contract FactoryERC721 {
+    ERC721[] public ERC721Array;
+    uint256 public amountOfContracts;
+    event NewContract(address contractAddress);
 
-   ERC721[] public ERC721Array;
-   uint256 public amountOfContracts;
-   event NewContract(address contractAddress);
+    function createNewContract(
+        string memory name,
+        string memory symbol
+    ) public {
+        ERC721Token newERC721 = new ERC721Token(name, symbol);
+        ERC721Array.push(newERC721);
+        amountOfContracts++;
+        emit NewContract(address(newERC721));
+    }
 
-   function createNewContract(string memory name, string memory symbol) public {
-     ERC721Token newERC721 = new ERC721Token(name,symbol);
-     ERC721Array.push(newERC721);
-     amountOfContracts++;
-     emit NewContract(address(newERC721));
-   }
+    function factoryMinter(
+        uint256 _contractIndex,
+        address _to,
+        uint256 _tokenId
+    ) public {
+        ERC721Token(address(ERC721Array[_contractIndex])).safeMint(
+            _to,
+            _tokenId
+        );
+    }
 
-   function factoryMinter(uint256 _contractIndex, address _to, uint256 _tokenId) public {
-     ERC721Token(address(ERC721Array[_contractIndex])).safeMint(_to,_tokenId);
-   }
-
-   function factoryOwnerOf(uint256 _contractIndex, uint256 _tokenId) public view returns (address) {
-    return ERC721Token(address(ERC721Array[_contractIndex])).ownerOf(_tokenId);
-   }
+    function factoryOwnerOf(
+        uint256 _contractIndex,
+        uint256 _tokenId
+    ) public view returns (address) {
+        return
+            ERC721Token(address(ERC721Array[_contractIndex])).ownerOf(_tokenId);
+    }
 }
